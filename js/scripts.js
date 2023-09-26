55a78aff78246d3c9bf5a5621747676eb52cad0d
shruthi <shruthisa.17@gmail.com>
2023-09-25 22:18:34 -0700

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,130 @@
+//STEP 1
+//var someMonth;
+//functionTheMonth 		
+//currentMonth 		    
+//var summerMonth; 		
+//myLibraryFunction    
+
+//STEP 2
+//"Take it easy"
+//657
+//false
+//null
+
+//STEP 3
+//var add = g + i;
+//var abc = 67 / 89;
+
+//STEP 4
+//var firstName;
+//var lastName;
+//var address;
+//var city;
+//var state;
+//var zipCode;
+//var age;
+//var referralSource;
+//var mayWeContactYou;
+
+//STEP 5
+//var firstName, address= "1678, Palomar Rd";
+//firstName = "Shruthi";
+//var lastName;
+//var city = "San Diego";
+//var state = "CA";
+//var zipCode = "92222";
+//var age = 29;
+//var referralSource;
+//referralSource = "Website";
+//var mayWeContactYou = true;
+
+//STEP 6
+//var address = 1678 + "Palomar Rd";
+//window.console.log(address);
+
+//STEP 7
+//var boolString = true + "";
+//var numBool = 562 + "";
+//window.console.log(boolString);
+//window.console.log(numBool);
+
+//STEP 8
+//var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//window.console.log(someString);
+
+//STEP 9
+//var var1 = null;
+//var var2;
+//window.console.log(var1);
+//window.console.log(var2);
+
+//STEP 10
+//var firstName = "Shruthi";
+//var num = 789;
+//var isTrue = false;
+//var baby = new Object();
+//var junkVar;
+//window.console.log(typeof firstName);
+//window.console.log(typeof num);
+//window.console.log(typeof isTrue);
+//window.console.log(typeof baby);
+//window.console.log(typeof junkVar);
+
+//STEP 11
+//window.alert("Hello " + "Zak Ruvalcaba , " + "welcome to the JavaScript class!");
+
+//STEP 12
+//var myName = "Shruthi Anandram";
+//window.alert("Hello " + myName + ", welcome to the JavaScript class!");
+
+//STEP 13
+//var myName = "Shruthi Anandram";
+//var course = "JavaScript";
+//window.alert("Hello " + myName + ", welcome to the " + course + " class!");
+
+//STEP 14
+//var myName = "Shruthi Anandram";
+//var course = "JavaScript";
+//window.alert("Hello " + myName + ".\nWelcome to the " + course + " class!");
+
+//STEP 15
+//var myName = window.prompt("What is your name?");
+//var course = "JavaScript";
+//window.alert("Hello " + myName + ".\nWelcome to the " + course + " class!");
+
+//STEP 16
+//var myName = window.prompt("What is your name?");
+//var course = window.prompt("What class are you taking?");
+//window.alert("Hello " + myName + ".\nWelcome to the " + course + " class!");
+
+//STEP 17
+//var x = 10;
+//var y = 20;
+//window.console.log(x + y);
+
+//STEP 18
+//var x = 20;
+//x = x + 20;
+//window.console.log(x);
+
+//STEP 19
+//var x = 20;
+//window.console.log(x*=5);
+
+//STEP 20
+//var x = 20 % 3;
+//window.console.log(x);
+
+//STEP 21
+//var x = 56;
+//var y = 66;
+//if (x != y && y > x) {
+//  window.console.log("This is true");
+//}
+
+//STEP 22
+//var m = 456;
+//var n = 789;
+//if (m == n || n > m) {
+//    window.console.log("false");
+//}
